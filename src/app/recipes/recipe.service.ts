c348e33cb1c1212e5814ea937d76cd2c14446e1c
danielb <daniel.berkovich@finantick.com>
2018-12-17 15:52:13 +0200

    added code

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,26 @@
 import {Recipe} from './recipes.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
     
     recipeSelected = new  EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('recipe name','simple recipe example','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
-        new Recipe('recipe name 2','simple recipe example','https://www.seriouseats.com/recipes/images/2017/06/20170627-summer-nachos-matt-clifton-1-1500x1125.jpg'),
+        new Recipe('Pizza',
+                    'Great Pizze to evening',
+                    'http://www.pngall.com/wp-content/uploads/2016/05/Pizza-Free-PNG-Image.png',
+                    [new Ingredient("cheeze",20),new Ingredient("dough",1)]),
+        new Recipe('Schnitzel'
+                    ,'Big Schnitzel for dinner',
+                    'http://pngimg.com/uploads/schnitzel/schnitzel_PNG7.png',
+                    [new Ingredient("Schnitzel",1),new Ingredient("Chips",20)]),
+        new Recipe('Burger'
+                   ,'Big Burger for dinner',
+                    'http://www.pngmart.com/files/1/Healthy-Burger-PNG.png',
+                     [new Ingredient("Buns",1),new Ingredient("Meat",1)]),
       ];
-
+      
     getRecipes(){
         return this.recipes.slice();
     }  
