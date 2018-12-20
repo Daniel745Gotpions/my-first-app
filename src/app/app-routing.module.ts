cc5d906570cb7e4e6acf7f62551e8d3f07745257
danielb <daniel.berkovich@finantick.com>
2018-12-20 14:08:55 +0200

    In progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
+import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
 
 const AppRoutes : Routes = [
     { path:'', redirectTo:'/recipes', pathMatch:'full'},
     { path: 'recipes', component:RecipesComponent ,children:[
-
+        {path:'',component:RecipesStartComponent},
+        {path:':id',component:RecipesDetailsComponent},
     ]},
     { path: 'shopping-list', component:ShoppingListComponent },
 ];
