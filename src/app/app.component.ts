8af635e0681a2efdf487272816f560680faf49d9
danielb <daniel.berkovich@finantick.com>
2018-12-20 13:54:30 +0200

    In progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  featureLoaded = 'recipe';
-  onNavigate(feature){
-    this.featureLoaded = feature;
-  }
+
 
 }
